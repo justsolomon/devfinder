0ee25010ff50f015ff0596896f5949e927d75341
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2021-10-25 14:14:45 +0100

    Add styles and markup for homepage

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">devfinder</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style lang="scss">
+.header {
+  margin-bottom: 24px;
+
+  &__title {
+    font-size: $font-size--2xl;
+    font-weight: $font-weight--bold;
+  }
+}
+</style>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="input-container">
+    <svg
+      class="input-container__icon"
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+    </svg>
+
+    <input
+      class="input-container__input"
+      placeholder="Search GitHub username..."
+    />
+
+    <button class="input-container__button">Search</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SearchInput",
+});
+</script>
+
+<style lang="scss">
+.input-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &__icon,
+  &__button {
+    position: absolute;
+  }
+
+  &__button,
+  &__input {
+    border-radius: $border-radius--lg;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  &__icon {
+    color: $brand-color;
+    height: 24px;
+    width: 24px;
+    left: 15px;
+  }
+
+  &__input {
+    height: 55px;
+    width: 100%;
+    padding: 0 90px 0 50px;
+    box-shadow: $shadow;
+    font-size: $font-size--md;
+
+    &::placeholder {
+      color: $secondary-color;
+    }
+  }
+
+  &__button {
+    height: 40px;
+    width: 80px;
+    right: 10px;
+    color: $white;
+    font-size: $font-size--base;
+    font-weight: $font-weight--bold;
+    background-color: $brand-color;
+
+    &:hover,
+    &:focus {
+      background-color: $brand-color-dark;
+    }
+  }
+
+  @media #{$media-mobile-sm} {
+    &__icon {
+      height: 20px;
+      width: 20px;
+    }
+
+    &__input,
+    &__button {
+      border-radius: $border-radius--md;
+    }
+
+    &__input {
+      height: 45px;
+      font-size: $font-size--sm;
+    }
+
+    &__button {
+      height: 35px;
+      width: 70px;
+      font-size: $font-size--sm;
+    }
+  }
+}
+</style>
